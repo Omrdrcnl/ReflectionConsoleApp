f6bce7c0d6fa6d98b1127f65ac12102e212d3fed
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-06-22 18:01:18 +0300

    ReflectionConsoleApp

---
+++ ReflectionConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Win32;
+
+namespace ReflectionConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            user user = new user();
+            var type = user.GetType();
+
+            foreach (var property in type.GetProperties())
+            {
+                Console.WriteLine($"{property.PropertyType}/{property.Name}");
+            }
+            Console.ReadLine();
+        }
+
+    }
+    public class user
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
+
